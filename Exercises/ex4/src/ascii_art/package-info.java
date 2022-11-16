33ffdb40a0925b2ce01691108e5a6acd37c3ad39
yair-shtern <101128645+yair-shtern@users.noreply.github.com>
2022-11-16 14:18:47 +0200

    Delete Exercises directory

--- Exercises/ex4/src/ascii_art/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Main module of the application.
- * @author Dan Nirel
- */
-package ascii_art;
--- Exercises/ex4/src/ascii_output/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The module responsible for outputting Ascii images.
- * @author Dan Nirel
- */
-package ascii_output;
